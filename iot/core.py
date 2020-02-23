b929aaabe7be2a466392af14e3d22cd07c5f6dfb
tserekh <kvertea@gmial.com>
2020-02-23 03:07:48 +0300

    alpha

--- iot/core.py
+++ iot/core.py
@@ -27,8 +27,7 @@ def get_bitalino_df(bitalino_path: str, N: int, lux_colname='A6', sync_col='sync
     df = df.reset_index().rename(columns={'index': 'exp_time'})
     df['exp_time'] = df['exp_time']/1000
     df['sysdatetime'] = datetime.strptime(dic['date'] + '_' + dic['time'], '%Y-%m-%d_%H:%M:%S.%f')  # .time()
-    df['sysdatetime'] = df['sysdatetime'] + df['exp_time'].apply(
-        lambda x: timedelta(seconds=x))
+    df['sysdatetime'] = df['sysdatetime'] + df['exp_time'].apply(lambda x: timedelta(seconds=x - 3600*3))
     return df, dic
 
 def get_glasses_df(glasses_path,  N=3000):
@@ -63,4 +62,36 @@ def get_tablet_df(tablet_path, N = 10000):
     df = pd.read_csv(tablet_path)
     df['sync_col'] = df['illuminance (lx)']/df['illuminance (lx)'].max()
     df['exp_time'] = (df['epoch (ms)'] - df['epoch (ms)'].min()) / 1000
+    df['sysdatetime'] = df['epoch (ms)'].apply(lambda x: datetime.utcfromtimestamp(x / 1000))
     return df, {}
+
+
+def get_opt_delta(df1, df2, deltas):
+    cols = ['sync_col', 'exp_time', 'sysdatetime']
+    df1['exp_time_round'] = np.round(df1['exp_time'], 1)
+    df2['exp_time_round'] = np.round(df2['exp_time'], 1)
+    errors = []
+    deltas_return = []
+    for delta in tqdm_notebook(deltas):
+        df_sub = pd.merge(df1, df2, on='exp_time_round')
+
+        df_sub['cumsum'] = df_sub['sync_col_x'].cumsum()
+        df_sub = df_sub[df_sub['cumsum'] != df_sub['cumsum'].min()]
+        df_sub = df_sub[df_sub['cumsum'] != df_sub['cumsum'].max()]
+
+        if df_sub.shape[0]:
+            error = mean_absolute_error(df_sub['sync_col_x'], df_sub['sync_col_y'])
+            errors.append(error)
+            deltas_return.append(delta)
+        else:
+            continue
+
+        if not errors:
+            return None, None
+    if not errors:
+        return None, None
+    errors = np.array(errors)
+    deltas_return = np.array(deltas_return)
+    min_error = errors.min()
+    delta = deltas_return[errors == min_error][0]
+    return delta, min_error
